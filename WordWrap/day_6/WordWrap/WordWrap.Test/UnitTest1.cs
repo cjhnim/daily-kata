da8e74d8b9b0068a3dd13aed7e2dfc891ffcb0aa
choi jung hyun <cjhnim@gmail.com>
2019-02-18 09:28:40 +0900

    day5: uncomplete

---
+++ WordWrap/day_6/WordWrap/WordWrap.Test/UnitTest1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WordWrap.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        WordWrapper wordWrapper;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            wordWrapper = new WordWrapper();
+        }
+        [TestMethod]
+        public void 공백이나_0값을_입력하면_wrap할필요가없다()
+        {
+            string output = wordWrapper.DoWrap("", 0);
+
+            Assert.AreEqual("", output);
+        }
+
+        [DataRow("word", "word", 4)]
+        [DataRow("word", "word", 5)]
+        [DataTestMethod]
+        [TestMethod]
+        public void 랩할것이없다(string Expected, string Actual, int col)
+        {
+            string output = wordWrapper.DoWrap(Actual, col);
+
+            Assert.AreEqual(Expected, output);
+        }
+
+        [DataRow("w--ord", "word", 1)]
+        [DataRow("wo--rd", "word", 2)]
+        [DataRow("wor--d", "word", 3)]
+        [DataTestMethod]
+        [TestMethod]
+        public void 한단어_랩(string Expected, string Actual, int col)
+        {
+            string output = wordWrapper.DoWrap(Actual, col);
+
+            Assert.AreEqual(Expected, output);
+        }
+
+        [DataRow("word--word", "word word", 4)]
+        [DataRow("word--word", "word word", 5)]
+        [DataTestMethod]
+        [TestMethod]
+        public void 두단어_랩(string Expected, string Actual, int col)
+        {
+            string output = wordWrapper.DoWrap(Actual, col);
+
+            Assert.AreEqual(Expected, output);
+        }
+
+        [DataRow("word--word", "word word", 6)]
+        [DataTestMethod]
+        [TestMethod]
+        public void 가능하면_단어를자르지않고_랩한다(string Expected, string Actual, int col)
+        {
+            string output = wordWrapper.DoWrap(Actual, col);
+
+            Assert.AreEqual(Expected, output);
+        }
+    }
+}
---
+++ WordWrap/day_6/WordWrap/WordWrap/WordWrapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WordWrap
+{
+    public class WordWrapper
+    {
+        public WordWrapper()
+        {
+        }
+
+        public string DoWrap(string input, int col)
+        {
+            if (string.IsNullOrEmpty(input) || col == 0)
+                return "";
+
+            if (input.Length > col)
+                return input.Substring(0, col).Trim() + "--" + input.Substring(col, input.Length- col).Trim();
+            else
+                return input;
+        }
+    }
+}
