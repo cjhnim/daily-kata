d696095bce1f5a85b01ca28b0cc26671d27ab8b4
choi jung hyun <cjhnim@gmail.com>
2019-03-11 10:04:58 +0900

    day12-complete

---
+++ WordWrap/day_12/WordWrap/WordWrap.Test/Wrapper.Test.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WordWrap.Test
+{
+    [TestClass]
+    public class WordwrapTest
+    {
+        [DataRow("", 0, "")]
+        [DataRow("word", 0, "word")]
+        [DataTestMethod]
+        public void 공백예외처리(string given, int limit, string expected)
+        {
+            Assert.AreEqual(expected, Wrapper.DoWrap(given, limit));
+        }
+
+        [DataRow("word", 4, "word")]
+        [DataRow("word", 5, "word")]
+        [DataRow("word", 6, "word")]
+        [DataTestMethod]
+        public void 한단어처리(string given, int limit, string expected)
+        {
+            Assert.AreEqual(expected, Wrapper.DoWrap(given, limit));
+        }
+
+        [DataRow("word", 2, "wo--rd")]
+        [DataRow("word wordwor", 6, "word w--ordwor")]
+        [DataTestMethod]
+        public void 랩크기가_단어보다_작으면_랩한다(string given, int limit, string expected)
+        {
+            Assert.AreEqual(expected, Wrapper.DoWrap(given, limit));
+        }
+
+        [DataRow("word word", 5, "word--word")]
+        [DataRow("word word", 4, "word--word")]
+        [DataTestMethod]
+        public void 두단어인경우_공백기준으로_랩한다(string given, int limit, string expected)
+        {
+            Assert.AreEqual(expected, Wrapper.DoWrap(given, limit));
+        }
+
+        [DataRow("word word word", 5, "word--word--word")]
+        [DataTestMethod]
+        public void 세단어도_동일한_규칙이_적용되어야한다(string given, int limit, string expected)
+        {
+            Assert.AreEqual(expected, Wrapper.DoWrap(given, limit));
+        }
+
+        [DataRow("test", "test", 7)]
+        [DataRow("hello--world", "hello world", 7)]
+        [DataRow("a lot of--words for--a single--line", "a lot of words for a single line", 10)]
+        [DataRow("this--is a--test", "this is a test", 4)]
+        [DataRow("a long--word", "a longword", 6)]
+        [DataRow("areall--ylongw--ord", "areallylongword", 6)]
+        [DataTestMethod]
+        public void 종합테스트(string Expected, string Given, int Length)
+        {
+            Assert.AreEqual(Expected, Wrapper.DoWrap(Given, Length));
+        }
+
+    }
+}
---
+++ WordWrap/day_12/WordWrap/WordWrap/Wrapper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace WordWrap
+{
+    public class Wrapper
+    {
+        public static string DoWrap(string text, int limit)
+        {
+            if (string.IsNullOrEmpty(text) || limit == 0)
+                return text;
+
+            return BreakLine(text, limit);
+        }
+
+        private static string BreakLine(string text, int limit)
+        {
+            if (limit >= text.Length)
+                return text;
+
+            int space = SpaceIndex(text, limit);
+
+            int wrapIndex = ChoiceWrapIndex(
+                text,
+                limit,
+                space);
+
+            return FrontString(text, wrapIndex) + "--" + BreakLine(RearString(text, wrapIndex).Trim(), limit);
+        }
+
+        private static string RearString(string text, int wrapIndex)
+        {
+            return text.Substring(wrapIndex);
+        }
+
+        private static string FrontString(string text, int wrapIndex)
+        {
+            return text.Substring(0, wrapIndex);
+        }
+
+        private static int SpaceIndex(string text, int limit)
+        {
+            return text.LastIndexOf(' ', limit);
+        }
+
+        private static int ChoiceWrapIndex(string text, int limit, int space)
+        {
+            if (limit >= WordLength(RearString(text, space + 1)))
+                return space;
+            return limit;
+        }
+
+        private static int WordLength(string text)
+        {
+            return text.Contains(" ") ? text.IndexOf(' ') + 1 : text.Length;
+        }
+    }
+}
