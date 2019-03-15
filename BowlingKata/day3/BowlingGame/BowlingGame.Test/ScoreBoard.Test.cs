b8aced4708df5731b59cdb56e1fcaa7b31796782
choi jung hyun <cjhnim@gmail.com>
2019-03-15 09:29:54 +0900

    day4: bowling kata complete

---
+++ BowlingKata/day3/BowlingGame/BowlingGame.Test/ScoreBoard.Test.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingGame.Test
+{
+    [TestClass]
+    public class ScoreBoardTests
+    {
+        ScoreBoard scoreBoard;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            scoreBoard = new ScoreBoard();
+        }
+
+        [TestMethod]
+        public void 모두0점을친다()
+        {
+            rollMany(count: 20, pins: 0);
+
+            Assert.AreEqual(0, scoreBoard.score());
+        }
+
+        [TestMethod]
+        public void 모두1점을친다()
+        {
+            rollMany(count: 20, pins: 1);
+
+            Assert.AreEqual(20, scoreBoard.score());
+        }
+
+        [TestMethod]
+        public void 스페어를친다()
+        {
+            scoreBoard.roll(5);
+            scoreBoard.roll(5);
+            scoreBoard.roll(5);
+            scoreBoard.roll(0);
+
+            rollMany(count: 16, pins: 0);
+
+            Assert.AreEqual(20, scoreBoard.score());
+        }
+
+        [TestMethod]
+        public void 모두스페어를친다()
+        {
+            rollMany(count: 21, pins: 5);
+
+            Assert.AreEqual(150, scoreBoard.score());
+        }
+
+        [TestMethod]
+        public void 스트라이크를친다()
+        {
+            scoreBoard.roll(10);
+            scoreBoard.roll(5);
+            scoreBoard.roll(4);
+
+            rollMany(count: 16, pins: 0);
+
+            Assert.AreEqual(28, scoreBoard.score());
+        }
+
+        [TestMethod]
+        public void 올스트라이크()
+        {
+            rollMany(count: 12, pins: 10);
+
+            Assert.AreEqual(300, scoreBoard.score());
+        }
+
+        private void rollMany(int count, int pins)
+        {
+            for (int i = 0; i < count; i++)
+                scoreBoard.roll(pins);
+        }
+    }
+}
---
+++ BowlingKata/day3/BowlingGame/BowlingGame/ScoreBoard.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace BowlingGame
+{
+    public class ScoreBoard
+    {
+        private const int MAX_COUNT = 21;
+        private const int MAX_FRAME = 10;
+        private const int TEN = 10;
+        int total;
+        int count;
+        int[] scores;
+
+        public ScoreBoard()
+        {
+            total = 0;
+            count = 0;
+            scores = new int[MAX_COUNT];
+        }
+
+        public void roll(int pins)
+        {
+            scores[count++] = pins;
+        }
+
+        public int score()
+        {
+            int i = 0;
+
+            for (int frame = 1; frame <= MAX_FRAME; frame++)
+            {
+                if (frame < MAX_FRAME && isStrike(i))
+                {
+                    total += TEN;
+                    total += strikeBonus(i);
+                    i += 1;
+                }
+                else
+                {
+                    if (frame < MAX_FRAME && isSpare(i))
+                        total += spareBonus(i);
+
+                    total += scores[i];
+                    total += scores[i + 1];
+                    i += 2;
+                }
+            }
+
+            total += scores[i];
+
+            return total;
+        }
+
+        private int spareBonus(int i)
+        {
+            return scores[i + 2];
+        }
+
+        private int strikeBonus(int i)
+        {
+            return scores[i + 1] + scores[i + 2];
+        }
+
+        private bool isSpare(int i)
+        {
+            return (scores[i] + scores[i + 1]) == TEN;
+        }
+
+        private bool isStrike(int i)
+        {
+            return scores[i] == TEN;
+        }
+    }
+}
