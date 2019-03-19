42f431ec23c6d44a4db74996d049d2a9ea3aadba
choi jung hyun <cjhnim@gmail.com>
2019-03-19 09:24:43 +0900

    day5: complete

---
+++ BowlingKata/day5/BowlingGame/BowlingGameTests/GameTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using BowlingGame;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingGameTests
+{
+    [TestClass]
+    public class GameTests
+    {
+        Game g;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            g = new Game();
+        }
+
+        private void RollMany(int rolls, int pins)
+        {
+            for (int i = 0; i < rolls; i++)
+                g.Roll(pins);
+        }
+
+        private void RollSpare()
+        {
+            g.Roll(5);
+            g.Roll(5);
+        }
+
+        private void RollStrike()
+        {
+            g.Roll(10);
+        }
+        
+        [TestMethod]
+        public void TestGutterGame()
+        {
+            RollMany(20, 0);
+
+            Assert.AreEqual(0, g.Score());
+        }
+
+        [TestMethod]
+        public void TestAllOnePins()
+        {
+            RollMany(20, 1);
+
+            Assert.AreEqual(20, g.Score());
+        }
+
+        [TestMethod]
+        public void TestOneSpare()
+        {
+            RollSpare();
+            g.Roll(3);
+
+            RollMany(17, 0);
+
+            Assert.AreEqual(16, g.Score());
+        }
+
+        [TestMethod]
+        public void TestOneStrike()
+        {
+            RollStrike();
+            g.Roll(5);
+            g.Roll(3);
+
+            RollMany(16, 0);
+
+            Assert.AreEqual(26, g.Score());
+        }
+
+        [TestMethod]
+        public void TestPerfectGame()
+        {
+            RollMany(12, 10);
+
+            Assert.AreEqual(300, g.Score());
+        }
+
+    }
+}
---
+++ BowlingKata/day5/BowlingGame/BowlingGameTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BowlingGameTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BowlingGameTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9d25d8ef-563d-4ac1-ba32-34c324924e83")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
