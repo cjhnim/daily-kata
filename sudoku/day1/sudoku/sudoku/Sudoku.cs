bf3e172db430dabf2ed0a7ffbe97b03ccb9b7507
choi jung hyun <cjhnim@gmail.com>
2019-04-09 14:20:24 +0900

    Sudoku 푸는 중

--- sudoku/day1/sudoku/sudoku/Sudoku.cs
+++ sudoku/day1/sudoku/sudoku/Sudoku.cs
@@ -1,16 +1,56 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace sudoku
 {
     public class Sudoku
     {
+        private const int EMPTY = 0;
+
         public Sudoku()
         {
         }
 
         public int[,] solve(int[,] question)
         {
-            throw new NotImplementedException();
+            var candidates = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int i;
+
+            // 수평 방향 경우의 수를 찾아라
+            for (i = 0; i < 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    if (question[0, i] == j)
+                        candidates.Remove(j);
+                }
+            }
+
+            // 빈 부분에 경우의 수를 넣어라
+            var hashTable = new Hashtable();
+
+            for (i = 0; i < 9; i++)
+            {
+                if (question[0, i] == EMPTY)
+                {
+                    Tuple<int, int> point = new Tuple<int, int>(0, i);
+                    hashTable.Add(point, candidates);
+                }
+            }
+
+            // 경우의 수 중 해답을 찾아라
+            for (i = 0; i < 9; i++)
+            {
+                if (question[0, i] == EMPTY)
+                {
+                    var key = new Tuple<int, int>(0, i);
+                    var value = (List<int>) hashTable[key];
+                    question[0, i] = value[0];
+                }
+            }
+
+            return question;
         }
 
         public bool validate(int[,] answer)
--- sudoku/day1/sudoku/sudokuTests/SudokuTest.cs
+++ sudoku/day1/sudoku/sudokuTests/SudokuTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using sudoku;
 
@@ -7,15 +8,79 @@ namespace sudokuTests
     [TestClass]
     public class SudokuTest
     {
-        [TestMethod]
-        public void Solve_and_Validator()
+        Sudoku sudoku;
+
+        int[,] expected = new int[9, 9] {
+                {5,3,4,6,7,8,9,1,2},
+                {6,7,2,1,9,5,3,4,8},
+                {1,9,8,3,4,2,5,6,7},
+                {8,5,9,7,6,1,4,2,3},
+                {4,2,6,8,5,3,7,9,1},
+                {7,1,3,9,2,4,8,5,6},
+                {9,6,1,5,3,7,2,8,4},
+                {2,8,7,4,1,9,6,3,5},
+                {3,4,5,2,8,6,1,7,9}
+                };
+
+        [TestInitialize]
+        public void SetUp()
         {
-            Sudoku sudoku = new Sudoku();
+            sudoku = new Sudoku();
+        }
 
-            int[,] question = new int[9, 9];
-            int[,] answer = sudoku.solve(question);
+        [TestMethod]
+        public void 답()
+        {
 
-            Assert.IsFalse(sudoku.validate(answer));
+            int[,] question = new int[9, 9] {
+                {5,3,4,6,7,8,9,1,2},
+                {6,7,2,1,9,5,3,4,8},
+                {1,9,8,3,4,2,5,6,7},
+                {8,5,9,7,6,1,4,2,3},
+                {4,2,6,8,5,3,7,9,1},
+                {7,1,3,9,2,4,8,5,6},
+                {9,6,1,5,3,7,2,8,4},
+                {2,8,7,4,1,9,6,3,5},
+                {3,4,5,2,8,6,1,7,9}
+                };
+
+            CollectionAssert.AreEqual(expected, sudoku.solve(question));
+        }
+
+        [TestMethod]
+        public void 하나풀기()
+        {
+            int[,] question = new int[9, 9] {
+                {0,3,4,6,7,8,9,1,2},
+                {6,7,2,1,9,5,3,4,8},
+                {1,9,8,3,4,2,5,6,7},
+                {8,5,9,7,6,1,4,2,3},
+                {4,2,6,8,5,3,7,9,1},
+                {7,1,3,9,2,4,8,5,6},
+                {9,6,1,5,3,7,2,8,4},
+                {2,8,7,4,1,9,6,3,5},
+                {3,4,5,2,8,6,1,7,9}
+                };
+
+            CollectionAssert.AreEqual(expected, sudoku.solve(question));
+        }
+
+        [TestMethod]
+        public void 하나풀기_옆으로()
+        {
+            int[,] question = new int[9, 9] {
+                {5,3,0,6,7,8,9,1,2},
+                {6,7,2,1,9,5,3,4,8},
+                {1,9,8,3,4,2,5,6,7},
+                {8,5,9,7,6,1,4,2,3},
+                {4,2,6,8,5,3,7,9,1},
+                {7,1,3,9,2,4,8,5,6},
+                {9,6,1,5,3,7,2,8,4},
+                {2,8,7,4,1,9,6,3,5},
+                {3,4,5,2,8,6,1,7,9}
+                };
+
+            CollectionAssert.AreEqual(expected, sudoku.solve(question));
         }
     }
 }
