a1a1e7494fdcaf0939882610aea4af1fd3fcb337
choi jung hyun <cjhnim@gmail.com>
2019-04-19 10:00:02 +0900

    sudoku: 경우의 수가 하나일때 문제 풀기

--- sudoku/day1/sudoku/sudoku/Sudoku.cs
+++ sudoku/day1/sudoku/sudoku/Sudoku.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace sudoku
 {
@@ -8,49 +9,95 @@ namespace sudoku
     {
         private const int EMPTY = 0;
 
+        public int[,] question
+        {
+            set;
+            get;
+        }
+
         public Sudoku()
         {
         }
 
-        public int[,] solve(int[,] question)
+        public int[,] Solve()
         {
-            var candidates = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            int i;
-
-            // 수평 방향 경우의 수를 찾아라
-            for (i = 0; i < 9; i++)
+            foreach (int yPos in Enumerable.Range(0, 9))
             {
-                for (int j = 1; j <= 9; j++)
+                foreach (int xPos in Enumerable.Range(0, 9))
                 {
-                    if (question[0, i] == j)
-                        candidates.Remove(j);
+                    if (question[xPos, yPos] == EMPTY)
+                    {
+                        List<int> result = SolveAtPosition(xPos, yPos);
+
+                        if (result.Count == 1)
+                            question[xPos, yPos] = result[0];
+                        else
+                            throw new NotImplementedException();
+                    }
                 }
             }
 
-            // 빈 부분에 경우의 수를 넣어라
-            var hashTable = new Hashtable();
 
-            for (i = 0; i < 9; i++)
-            {
-                if (question[0, i] == EMPTY)
-                {
-                    Tuple<int, int> point = new Tuple<int, int>(0, i);
-                    hashTable.Add(point, candidates);
-                }
-            }
+            //// 빈 부분에 경우의 수를 넣어라
+            //var hashTable = new Hashtable();
 
-            // 경우의 수 중 해답을 찾아라
-            for (i = 0; i < 9; i++)
-            {
-                if (question[0, i] == EMPTY)
-                {
-                    var key = new Tuple<int, int>(0, i);
-                    var value = (List<int>) hashTable[key];
-                    question[0, i] = value[0];
-                }
-            }
+            //for (i = 0; i < 9; i++)
+            //{
+            //    if (question[0, i] == EMPTY)
+            //    {
+            //        Tuple<int, int> point = new Tuple<int, int>(0, i);
+            //        hashTable.Add(point, candidates);
+            //    }
+            //}
+
+            //// 경우의 수 중 해답을 찾아라
+            //for (i = 0; i < 9; i++)
+            //{
+            //    if (question[0, i] == EMPTY)
+            //    {
+            //        var key = new Tuple<int, int>(0, i);
+            //        var value = (List<int>)hashTable[key];
+            //        question[0, i] = value[0];
+            //    }
+            //}
 
             return question;
+        }
+
+        private List<int> SolveAtPosition(int xPos, int yPos)
+        {
+
+
+            var numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            var vSlice = Enumerable.Range(0, 9)
+                .Select(x => question[xPos, x])
+                .ToList<int>();
+
+            var candidatesX = numbers.Except(vSlice).ToList();
+
+
+            // 수직 조각 답찾기
+            var hSlice = Enumerable.Range(0, 9)
+                .Select(x => question[x, yPos])
+                .ToList<int>();
+
+            var candidatesY = numbers.Except(hSlice).ToList();
+
+            var result = candidatesX.Intersect(candidatesY).ToList();
+
+            // 센터 조각 답찾기
+            var cSlice = new List<int>();
+            uint startX = ((uint) xPos / 3)*3;
+            uint startY = ((uint) yPos / 3)*3;
+            foreach (int x in Enumerable.Range((int)startX, 3))
+                foreach (int y in Enumerable.Range((int)startY, 3))
+                    cSlice.Add(question[x, y]);
+
+            var candidatesCenter = numbers.Except(cSlice).ToList();
+
+            result = result.Intersect(candidatesCenter).ToList();
+            return result;
         }
 
         public bool validate(int[,] answer)
--- sudoku/day1/sudoku/sudokuTests/SudokuTest.cs
+++ sudoku/day1/sudoku/sudokuTests/SudokuTest.cs
@@ -8,8 +8,6 @@ namespace sudokuTests
     [TestClass]
     public class SudokuTest
     {
-        Sudoku sudoku;
-
         int[,] expected = new int[9, 9] {
                 {5,3,4,6,7,8,9,1,2},
                 {6,7,2,1,9,5,3,4,8},
@@ -25,14 +23,13 @@ namespace sudokuTests
         [TestInitialize]
         public void SetUp()
         {
-            sudoku = new Sudoku();
         }
 
         [TestMethod]
         public void 답()
         {
-
-            int[,] question = new int[9, 9] {
+            var sudoku = new Sudoku {
+                question = new int[9, 9] {
                 {5,3,4,6,7,8,9,1,2},
                 {6,7,2,1,9,5,3,4,8},
                 {1,9,8,3,4,2,5,6,7},
@@ -42,15 +39,18 @@ namespace sudokuTests
                 {9,6,1,5,3,7,2,8,4},
                 {2,8,7,4,1,9,6,3,5},
                 {3,4,5,2,8,6,1,7,9}
-                };
+                }
+            };
 
-            CollectionAssert.AreEqual(expected, sudoku.solve(question));
+            CollectionAssert.AreEqual(expected, sudoku.Solve());
         }
 
         [TestMethod]
-        public void 하나풀기()
+        public void 수평검색_1개()
         {
-            int[,] question = new int[9, 9] {
+            var sudoku = new Sudoku
+            {
+                question = new int[9, 9] {
                 {0,3,4,6,7,8,9,1,2},
                 {6,7,2,1,9,5,3,4,8},
                 {1,9,8,3,4,2,5,6,7},
@@ -60,16 +60,19 @@ namespace sudokuTests
                 {9,6,1,5,3,7,2,8,4},
                 {2,8,7,4,1,9,6,3,5},
                 {3,4,5,2,8,6,1,7,9}
-                };
+                }
+            };
 
-            CollectionAssert.AreEqual(expected, sudoku.solve(question));
+            Assert.AreEqual(expected[0, 0], sudoku.Solve()[0, 0]);
         }
 
         [TestMethod]
-        public void 하나풀기_옆으로()
+        public void 수평수직검색()
         {
-            int[,] question = new int[9, 9] {
-                {5,3,0,6,7,8,9,1,2},
+            var sudoku = new Sudoku
+            {
+                question = new int[9, 9] {
+                {0,3,4,0,7,8,9,1,2},
                 {6,7,2,1,9,5,3,4,8},
                 {1,9,8,3,4,2,5,6,7},
                 {8,5,9,7,6,1,4,2,3},
@@ -78,9 +81,94 @@ namespace sudokuTests
                 {9,6,1,5,3,7,2,8,4},
                 {2,8,7,4,1,9,6,3,5},
                 {3,4,5,2,8,6,1,7,9}
-                };
+                }
+            };
 
-            CollectionAssert.AreEqual(expected, sudoku.solve(question));
+            Assert.AreEqual(expected[0, 0], sudoku.Solve()[0, 0]);
         }
+
+        [TestMethod]
+        public void 수평수직검색2()
+        {
+            var sudoku = new Sudoku
+            {
+                question = new int[9, 9] {
+                {0,3,4,0,7,8,9,1,2},
+                {6,7,2,1,9,5,3,4,8},
+                {1,9,8,3,4,2,5,6,7},
+                {0,5,9,7,6,1,4,2,3},
+                {4,2,6,8,5,3,7,9,1},
+                {7,1,3,9,2,4,8,5,6},
+                {9,6,1,5,3,7,2,8,4},
+                {2,8,7,4,1,9,6,3,5},
+                {3,4,5,2,8,6,1,7,9}
+                }
+            };
+
+            Assert.AreEqual(expected[0, 0], sudoku.Solve()[0, 0]);
+        }
+
+        [TestMethod]
+        public void 수평수직센터검색()
+        {
+            var sudoku = new Sudoku
+            {
+                question = new int[9, 9] {
+                {0,3,4,6,0,8,9,1,2},
+                {6,7,2,1,9,5,3,4,8},
+                {1,9,8,3,4,2,5,6,7},
+                {8,5,9,7,6,1,4,2,3},
+                {4,2,6,8,5,3,7,9,1},
+                {0,1,3,9,2,4,8,5,6},
+                {9,6,1,5,3,7,2,8,4},
+                {2,8,7,4,1,9,6,3,5},
+                {3,4,5,2,8,6,1,7,9}
+                }
+            };
+
+            Assert.AreEqual(expected[0, 0], sudoku.Solve()[0, 0]);
+        }
+
+
+        [TestMethod]
+        public void 임의좌표()
+        {
+            var sudoku = new Sudoku
+            {
+                question = new int[9, 9] {
+                {5,3,4,6,7,8,9,1,2},
+                {6,0,2,1,9,5,3,4,8},
+                {1,9,8,3,4,2,5,6,7},
+                {8,5,9,7,6,1,4,2,3},
+                {4,2,6,8,5,3,7,9,1},
+                {7,1,3,9,2,4,8,5,6},
+                {9,6,1,5,3,7,2,8,4},
+                {2,8,7,4,1,9,6,3,5},
+                {3,4,5,2,8,6,1,7,9}
+                }
+            };
+
+            CollectionAssert.AreEqual(expected, sudoku.Solve());
+        }
+
+
+
+        //[TestMethod]
+        //public void 수평수직검색_두개의해에서답찾기()
+        //{
+        //    int[,] question = new int[9, 9] {
+        //        {0,0,4,6,7,8,9,1,2},
+        //        {6,7,2,1,9,5,3,4,8},
+        //        {1,9,8,3,4,2,5,6,7},
+        //        {8,5,9,7,6,1,4,2,3},
+        //        {4,2,6,8,5,3,7,9,1},
+        //        {7,1,3,9,2,4,8,5,6},
+        //        {9,6,1,5,3,7,2,8,4},
+        //        {2,8,7,4,1,9,6,3,5},
+        //        {3,4,5,2,8,6,1,7,9}
+        //        };
+
+        //    CollectionAssert.AreEqual(expected, sudoku.solve(question));
+        //}
     }
 }
