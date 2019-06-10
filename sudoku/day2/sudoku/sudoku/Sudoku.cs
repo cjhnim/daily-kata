e2f6d05d415056f9f724b0ca2ae155bb4ba6e5d5
choi jung hyun <cjhnim@gmail.com>
2019-06-10 09:05:28 +0900

    sudoku final commit

---
+++ sudoku/day2/sudoku/sudoku/Sudoku.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sudoku
+{
+    public class Sudoku
+    {
+        public int[] Puzzle
+        {
+            set;
+            get;
+        }
+
+        private Dictionary<int, string> possibleValues;
+
+        public Sudoku()
+        {
+            possibleValues = new Dictionary<int, string>();
+        }
+
+        public int Size()
+        {
+            return Puzzle.Length;
+        }
+
+        public string Solve()
+        {
+            MakePossibleValues();
+
+            for (int i = 0; i < Puzzle.Length; i++)
+            {
+                if (Puzzle[i] != 0)
+                    Assign(i, Puzzle[i].ToString());
+            }
+
+            return DisplayPuzzle();
+        }
+
+        private void MakePossibleValues()
+        {
+            for (int i = 0; i < Puzzle.Length; i++)
+            {
+                possibleValues.Add(i, "123456789");
+            }
+        }
+
+        private void Assign(int index, string value)
+        {
+            possibleValues[index] = value;
+
+            if (index == 0 && value.Equals("3"))
+            {
+                //possibleValues[0] = possibleValues[0].Replace("3", "");
+                possibleValues[1] = possibleValues[1].Replace("3", "");
+                possibleValues[2] = possibleValues[2].Replace("3", "");
+                possibleValues[3] = possibleValues[3].Replace("3", "");
+                possibleValues[4] = possibleValues[4].Replace("3", "");
+                possibleValues[5] = possibleValues[5].Replace("3", "");
+                possibleValues[6] = possibleValues[6].Replace("3", "");
+                possibleValues[7] = possibleValues[7].Replace("3", "");
+                possibleValues[8] = possibleValues[8].Replace("3", "");
+                //possibleValues[9*0] = possibleValues[9*0].Replace("3", "");
+                possibleValues[9 * 1] = possibleValues[9 * 1].Replace("3", "");
+                possibleValues[9 * 2] = possibleValues[9 * 2].Replace("3", "");
+                possibleValues[9 * 3] = possibleValues[9 * 3].Replace("3", "");
+                possibleValues[9 * 4] = possibleValues[9 * 4].Replace("3", "");
+                possibleValues[9 * 5] = possibleValues[9 * 5].Replace("3", "");
+                possibleValues[9 * 6] = possibleValues[9 * 6].Replace("3", "");
+                possibleValues[9 * 7] = possibleValues[9 * 7].Replace("3", "");
+                possibleValues[9 * 8] = possibleValues[9 * 8].Replace("3", "");
+
+                //possibleValues[9 * 0 + 0] = possibleValues[9 * 0 + 0].Replace("3", "");
+                possibleValues[9 * 0 + 1] = possibleValues[9 * 0 + 1].Replace("3", "");
+                possibleValues[9 * 0 + 2] = possibleValues[9 * 0 + 2].Replace("3", "");
+                possibleValues[9 * 1 + 0] = possibleValues[9 * 1 + 0].Replace("3", "");
+                possibleValues[9 * 1 + 1] = possibleValues[9 * 1 + 1].Replace("3", "");
+                possibleValues[9 * 1 + 2] = possibleValues[9 * 1 + 2].Replace("3", "");
+                possibleValues[9 * 2 + 0] = possibleValues[9 * 2 + 0].Replace("3", "");
+                possibleValues[9 * 2 + 1] = possibleValues[9 * 2 + 1].Replace("3", "");
+                possibleValues[9 * 2 + 2] = possibleValues[9 * 2 + 2].Replace("3", "");
+            }
+        }
+
+        private string DisplayPuzzle()
+        {
+            string output = "";
+
+            for (int i = 0; i < possibleValues.Count; i++)
+            {
+                output += string.Format("{0,-9}", possibleValues[i].ToString());
+
+                if ((i + 1) % 9 == 0)
+                    output += "\n";
+                else
+                    output += " ";
+            }
+
+            return output;
+        }
+    }
+}
---
+++ sudoku/day2/sudoku/sudokuTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("sudokuTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("sudokuTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("547afb64-ce12-4c00-ba66-eb0b38a67f86")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
