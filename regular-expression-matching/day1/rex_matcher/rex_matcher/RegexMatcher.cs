7fc4f805957c6a6db7f50835ed7b86942c99a429
choi jung hyun <cjhnim@gmail.com>
2019-07-04 09:03:02 +0900

    RegMatcher: Dynamic Programming by Bottom Up

--- regular-expression-matching/day1/rex_matcher/rex_matcher/RegexMatcher.cs
+++ regular-expression-matching/day1/rex_matcher/rex_matcher/RegexMatcher.cs
@@ -15,7 +15,33 @@ namespace rex_matcher
 
         public bool IsMatch(string Text, string Pattern)
         {
-            return IsMatchRecursively(Text, Pattern);
+            //return IsMatchRecursively(Text, Pattern);
+            return IsMatchDPBottomUp(Text, Pattern);
+        }
+
+        public bool IsMatchDPBottomUp(string text, string pattern)
+        {
+            var dp = new bool[text.Length + 1, pattern.Length + 1];
+            dp[text.Length, pattern.Length] = true;
+
+            for (int i = text.Length; i >= 0; i--)
+            {
+                for (int j = pattern.Length - 1; j >= 0; j--)
+                {
+                    bool first_match = (i < text.Length &&
+                                           (pattern[j] == text[i] ||
+                                            pattern[j] == '.'));
+                    if (j + 1 < pattern.Length && pattern[j + 1] == '*')
+                    {
+                        dp[i, j] = dp[i, j + 2] || first_match && dp[i + 1, j];
+                    }
+                    else
+                    {
+                        dp[i, j] = first_match && dp[i + 1, j + 1];
+                    }
+                }
+            }
+            return dp[0, 0];
         }
 
         private bool IsMatchRecursively(string Text, string Pattern)
