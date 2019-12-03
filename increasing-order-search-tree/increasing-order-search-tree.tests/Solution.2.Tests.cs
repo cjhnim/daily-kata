77abb3aef95032927c24cbe5932c391e6110ece0
choi jung hyun <cjhnim@gmail.com>
2019-12-03 09:19:59 +0900

    increasing-order-search-tree: solved by other way

---
+++ increasing-order-search-tree/increasing-order-search-tree.tests/Solution.2.Tests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace increasing_order_search_tree.tests
+{
+    [TestClass]
+    public class Solution2Tests
+    {
+        Solution2 sol2;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            sol2 = new Solution2();
+        }
+
+        [TestMethod]
+        public void nullItem()
+        {
+            TreeNode root = null;
+            TreeNode result = sol2.IncreasingBST(root);
+
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void oneItem()
+        {
+            TreeNode root = new TreeNode(1);
+            TreeNode result = sol2.IncreasingBST(root);
+
+            LeafNodeAssertAreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void twoItemOfLeft()
+        {
+            TreeNode root = new TreeNode(2);
+            root.left = new TreeNode(1);
+
+            TreeNode result = sol2.IncreasingBST(root);
+
+            NodeAssertAreEqual(1, result);
+            LeafNodeAssertAreEqual(2, result.right);
+        }
+
+        [TestMethod]
+        public void threeItemOfRight()
+        {
+            TreeNode root = new TreeNode(2);
+            root.right = new TreeNode(4);
+            root.right.left = new TreeNode(3);
+
+            TreeNode result = sol2.IncreasingBST(root);
+
+            NodeAssertAreEqual(2, result);
+            NodeAssertAreEqual(3, result.right);
+            LeafNodeAssertAreEqual(4, result.right.right);
+        }
+
+        private static void NodeAssertAreEqual(int expectedVal, TreeNode actual)
+        {
+            Assert.AreEqual(expectedVal, actual.val);
+            Assert.IsNull(actual.left);
+            Assert.IsNotNull(actual.right);
+        }
+
+        private static void LeafNodeAssertAreEqual(int expectedVal, TreeNode actual)
+        {
+            Assert.AreEqual(expectedVal, actual.val);
+            Assert.IsNull(actual.left);
+            Assert.IsNull(actual.right);
+        }
+    }
+}
---
+++ increasing-order-search-tree/increasing-order-search-tree/Solution2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace increasing_order_search_tree
+{
+    public class Solution2
+    {
+        TreeNode current;
+
+        public Solution2()
+        {
+        }
+
+        public TreeNode IncreasingBST(TreeNode root)
+        {
+            if (root == null)
+                return root;
+
+            TreeNode head = new TreeNode(0);
+
+            head.right = root;
+            current = head;
+            inorder(root);
+
+            return head.right;
+        }
+
+        private void inorder(TreeNode node)
+        {
+            if (node == null)
+                return;
+
+            leftTraverse(node);
+            centerTraverse(node);
+            rightTraverse(node);
+        }
+
+        private void rightTraverse(TreeNode node)
+        {
+            inorder(node.right);
+        }
+
+        private void centerTraverse(TreeNode node)
+        {
+            current.right = node;
+            current = node;
+            node.left = null;
+        }
+
+        private void leftTraverse(TreeNode node)
+        {
+            inorder(node.left);
+        }
+    }
+}
