21f1d387be9e854ff57b702ba1c13e4ceacdd752
choi jung hyun <cjhnim@gmail.com>
2020-01-15 09:29:40 +0900

    diamond kata solved by recyling tests

---
+++ diamond-kata/diamond-kata.tests/DiamondTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace diamond_kata.tests
+{
+    [TestClass]
+    public class DiamondTests
+    {
+        [TestMethod]
+        public void A()
+        {
+            Diamond diamond = new Diamond('A');
+            string expected = "A\n";
+
+            Assert.AreEqual(expected, diamond.Print());
+        }
+
+        [TestMethod]
+        public void B()
+        {
+            Diamond diamond = new Diamond('B');
+            string expected = 
+                " A\n" +
+                "B B\n" +
+                " A\n";
+
+            Assert.AreEqual(expected, diamond.Print());
+        }
+
+        [TestMethod]
+        public void C()
+        {
+            Diamond diamond = new Diamond('C');
+            string expected =
+                "  A\n" +
+                " B B\n" +
+                "C   C\n" +
+                " B B\n" +
+                "  A\n";
+
+            Assert.AreEqual(expected, diamond.Print());
+        }
+
+        [TestMethod]
+        public void D()
+        {
+            Diamond diamond = new Diamond('D');
+            string expected =
+                "   A\n" +
+                "  B B\n" +
+                " C   C\n" +
+                "D     D\n" +
+                " C   C\n" +
+                "  B B\n" +
+                "   A\n";
+
+            Assert.AreEqual(expected, diamond.Print());
+        }
+    }
+}
---
+++ diamond-kata/diamond-kata/Diamond.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diamond_kata
+{
+    public class Diamond
+    {
+        private char mid;
+
+        public Diamond(char character)
+        {
+            this.mid = character;
+        }
+
+        public string Print()
+        {
+            string result = "";
+            string top = "";
+            string bottom = "";
+
+            for (char c = 'A'; c <= mid; c++)
+            {
+                string line = BuildLine(result, c);
+
+                top += line;
+
+                if (mid == c)
+                    continue;
+
+                bottom = line + bottom;
+            }
+
+            return top + bottom;
+        }
+
+        private string BuildLine(string result, char c)
+        {
+            return AddIndent(result, c) + c + AddRest(result, c) + "\n";
+        }
+
+        private string AddIndent(string result, char c)
+        {
+            if (c != mid)
+                result += AddSpace(mid - c);
+            return result;
+        }
+
+        private string AddSpace(int count)
+        {
+            return new string(' ', count);
+        }
+
+        private string AddRest(string result, char current)
+        {
+            if (current != 'A')
+                result += AddSpace(SpaceCount(current)) + current;
+
+            return result;
+        }
+
+        private int SpaceCount(char current)
+        {
+            int index = current - 'A';
+            int count = index * 2 - 1;
+            return count;
+        }
+    }
+}
